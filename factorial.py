37491bb92c3555b742b5640957bd9072d47a28e8
asapelcin <13572@holbertonstudents.com>
2026-03-14 14:39:56 +0400

    Add argument check for factorial calculation
    Add input check for command line arguments before calculating factorial.

--- factorial.py
+++ factorial.py
@@ -5,7 +5,10 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
+        n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if len(sys.argv) > 1:
+    f = factorial(int(sys.argv[1]))
+    print(f)
+EOF
